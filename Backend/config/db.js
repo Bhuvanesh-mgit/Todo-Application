d01379bc44ee3ce6b2ce70a75490fde1d8cc35ed
Bhuvanesh-mgit <bhuvanesh.m444@gmail.com>
2026-06-08 19:58:31 +0530

    Update MongoDB connection string in db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     let connect = await mongoose.connect(
-      "mongodb://bhuvaneshm444_db_user:3t5mdC1v1oG7iIsp@ac-vzr9ltt-shard-00-00.89kbhnr.mongodb.net:27017,ac-vzr9ltt-shard-00-01.89kbhnr.mongodb.net:27017,ac-vzr9ltt-shard-00-02.89kbhnr.mongodb.net:27017/?ssl=true&replicaSet=atlas-17fwdh-shard-0&authSource=admin&appName=Cluster0",
+    
     );
     console.log("Db Connected");
   } catch (error) {
@@ -13,4 +13,4 @@ const connectDb = async () => {
 
 export default connectDb;
 
-// mongodb+srv://bhuvaneshm444_db_user:3t5mdC1v1oG7iIsp@cluster0.89kbhnr.mongodb.net/?appName=Cluster0
+
